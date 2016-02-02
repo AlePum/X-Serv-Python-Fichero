77eee312dc029b2f0cc421e4f003b1ec57cfe3c5
alejandro franco garcia <afranco@zeta40.aulas.gsyc.es>
2016-02-02 10:46:45 +0100

    Fichero de ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
+fl = open("/etc/passwd" , "r")
+lineas = fl.readlines()
+for linea in lineas:
+    print "->Username ", linea.split(':')[0], " ->Shell ", linea.split(':')[6]
+print "Hay ", len(lineas), " usuarios"
